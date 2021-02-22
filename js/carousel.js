4eefe3e3d2a7c0d9555b91daf737c15cd1142deb
RomanRDQ <roood90@gmail.com>
2021-02-22 14:08:45 +0200

    ASD

--- js/carousel.js
+++ js/carousel.js
@@ -1,35 +1,31 @@
 const images = [
-
-  'img/carousel-img/adidas.png',
+  'img/carousel-img/adidas-1500.png',
   'img/carousel-img/aerodyne.png',
   'img/carousel-img/aeroplane.png',
   'img/carousel-img/akando.png',
-  'img/carousel-img/just-do-it.png'
-  
-]
+  'img/carousel-img/just-do-it.png',
+];
 
 let currIdx = 0;
 
-function showCurrentImage(){
-  const imgContainer = document.querySelector('.curr')
+function showCurrentImage() {
+  const imgContainer = document.querySelector(".curr");
   imgContainer.src = images[currIdx];
 }
 
 showCurrentImage();
 
-function nextImage(){
-  currIdx++
+function nextImage() {
+  currIdx++;
   if (currIdx >= images.length) currIdx = 0;
   showCurrentImage();
-};
+}
 
-
-function prevImage(){
+function prevImage() {
   currIdx--;
   if (currIdx < 0) currIdx = images.length - 1;
   showCurrentImage();
 }
 
-
-document.querySelector('prev').addEventListener('click', prevImage);
-document.querySelector('next').addEventListener('click', nextImage);
+document.querySelector(".prev").addEventListener("click", prevImage);
+document.querySelector(".next").addEventListener("click", nextImage);
