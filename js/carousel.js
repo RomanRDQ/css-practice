659085e77ecdaca35d5ac62925946c2cbaa96d2a
RomanRDQ <roood90@gmail.com>
2021-02-23 17:48:03 +0200

    gh-pages

--- js/carousel.js
+++ js/carousel.js
@@ -1,9 +1,9 @@
 const images = [
-  'img/carousel-img/karusel-adidas.jpg',
-  'img/carousel-img/karusel-aerodyne.jpg',
-  'img/carousel-img/karusel-aeroplane.jpg',
-  'img/carousel-img/karusel-akando.jpg',
-  'img/carousel-img/karusel-justdoit.jpg'
+  'img/carousel-img/adidas.png',
+  'img/carousel-img/aerodyne.png',
+  'img/carousel-img/aeroplane.png',
+  'img/carousel-img/akando.png',
+  'img/carousel-img/just-do-it.png'
 ];
 
 let currIdx = 0;
